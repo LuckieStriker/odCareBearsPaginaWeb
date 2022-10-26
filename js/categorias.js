4f81aa05877ea84e7b5fcc50368d716343b50df2
Miguel Morán <miguelrmor@gmail.com>
2022-10-26 18:54:08 -0500

    add: enlaces a diferentes categorías en el index

--- js/categorias.js
+++ js/categorias.js
@@ -1,75 +1,75 @@
-const products=[
+const products = [
     {
         "categoria": "ositos",
         "nombre": "Ositos Cariñositos",
-        "productos":[
+        "productos": [
             {
-                "categoria":"ositos",
-                "nombre":"Osos",
-                "precio":199.00,
-                "descuento":100.00,
-                "imagen":"Osos.jpg"
+                "categoria": "ositos",
+                "nombre": "Osos",
+                "precio": 199.00,
+                "descuento": 100.00,
+                "imagen": "Osos.jpg"
             },
             {
-                "categoria":"ositos",
-                "nombre":"Oso azul",
-                "precio":199.00,
-                "descuento":100.00,
-                "imagen":"OsoAzul.jpg"
+                "categoria": "ositos",
+                "nombre": "Oso azul",
+                "precio": 199.00,
+                "descuento": 100.00,
+                "imagen": "OsoAzul.jpg"
             },
             {
-                "categoria":"ositos",
-                "nombre":"Oso café",
-                "precio":199.00,
-                "descuento":100.00,
-                "imagen":"OsoCafe.jpg"
+                "categoria": "ositos",
+                "nombre": "Oso café",
+                "precio": 199.00,
+                "descuento": 100.00,
+                "imagen": "OsoCafe.jpg"
             },
             {
-                "categoria":"ositos",
-                "nombre":"Oso morado",
-                "precio":199.00,
-                "descuento":100.00,
-                "imagen":"OsoMorado.jpg"
+                "categoria": "ositos",
+                "nombre": "Oso morado",
+                "precio": 199.00,
+                "descuento": 100.00,
+                "imagen": "OsoMorado.jpg"
             },
             {
-                "categoria":"ositos",
-                "nombre":"Oso rosa",
-                "precio":199.00,
-                "descuento":100.00,
-                "imagen":"OsoRosa.jpg"
+                "categoria": "ositos",
+                "nombre": "Oso rosa",
+                "precio": 199.00,
+                "descuento": 100.00,
+                "imagen": "OsoRosa.jpg"
             },
             {
-                "categoria":"ositos",
-                "nombre":"Oso rosita",
-                "precio":199.00,
-                "descuento":100.00,
-                "imagen":"OsoRosita.jpg"
+                "categoria": "ositos",
+                "nombre": "Oso rosita",
+                "precio": 199.00,
+                "descuento": 100.00,
+                "imagen": "OsoRosita.jpg"
             },
 
 
-        ] 
-    },
-    {
-        "categoria":"kawaii",
-        "nombre":"Kawaii",
-        "productos":[
-            
         ]
     },
     {
-        "categoria":"ceramica",
-        "nombre":"Cerámica",
-        "productos":[
-            
+        "categoria": "kawaii",
+        "nombre": "Kawaii",
+        "productos": [
+
         ]
     },
     {
-        "categoria":"otros",
-        "nombre":"Otros",
-        "productos":[
-            
+        "categoria": "ceramica",
+        "nombre": "Cerámica",
+        "productos": [
+
         ]
-    } 
+    },
+    {
+        "categoria": "otros",
+        "nombre": "Otros",
+        "productos": [
+
+        ]
+    }
 ]
 
 function getParameterByName(name, url = window.location.href) {
@@ -81,34 +81,34 @@ function getParameterByName(name, url = window.location.href) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-function getCategoria(categoriaPath){
+function getCategoria(categoriaPath) {
     let categoria = null;
-    products.forEach(element=>{
-        if(element.categoria===categoriaPath){
-            categoria= element;
+    products.forEach(element => {
+        if (element.categoria === categoriaPath) {
+            categoria = element;
         }
     });
     return categoria;
 }
-let categoriaPath = getParameterByName('categoria'); 
-let categoria=getCategoria(categoriaPath);
+let categoriaPath = getParameterByName('categoria');
+let categoria = getCategoria(categoriaPath);
 
-function pintarTitulo(){
-    document.getElementById('titulo-h1').innerHTML=categoria.nombre;
+function pintarTitulo() {
+    document.getElementById('titulo-h1').innerHTML = categoria.nombre;
 }
 
-function pintarProductos(){
-    let html="";
-    categoria.productos.forEach(element=>{
-        let fila='<div class="col-md-3"><a target="_blank"><img src="{imagen}" class="img-fluid rounded-4" alt=""></a><p align="center">{nombre}<br> {precioConDescuento}MXN   <s>{precioSinDescuento}MXN</s></p></div>';
-        fila=fila.replace("{imagen}",'../assets/img/'+categoriaPath+'/'+element.imagen);
-        fila=fila.replace("{nombre}",element.nombre);
-        fila=fila.replace("{precioConDescuento}",element.precio-element.descuento);
-        fila=fila.replace("{precioSinDescuento}",element.precio);
+function pintarProductos() {
+    let html = "";
+    categoria.productos.forEach(element => {
+        let fila = '<div class="col-md-3"><a target="_blank"><img src="{imagen}" class="img-fluid rounded-4" alt=""></a><p align="center">{nombre}<br> {precioConDescuento}MXN   <s>{precioSinDescuento}MXN</s></p></div>';
+        fila = fila.replace("{imagen}", '../assets/img/' + categoriaPath + '/' + element.imagen);
+        fila = fila.replace("{nombre}", element.nombre);
+        fila = fila.replace("{precioConDescuento}", element.precio - element.descuento);
+        fila = fila.replace("{precioSinDescuento}", element.precio);
         console.log(fila);
-        html+=fila;
+        html += fila;
     })
-    document.getElementById('fila-1').innerHTML=html;
+    document.getElementById('fila-1').innerHTML = html;
 }
 pintarTitulo();
 pintarProductos();
