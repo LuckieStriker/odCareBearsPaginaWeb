fcdce429010170be09eaafccc33fa1b52ef3fa81
Miguel Morán <miguelrmor@gmail.com>
2022-10-25 19:03:50 -0500

    add: intento de json imágenes de osos en JS

---
+++ js/categorias.js
@@ -0,0 +1,24 @@
+
+XMLHttpRequest()
+
+
+
+
+function cargaDatos(){
+    let data= (../)
+}
+
+
+
+
+function productos(){
+    document.getElementById('fila-1').innerHTML;
+    let imagenes;
+    let html="";
+    datos.data.array.forEach(element => {
+        let img="<td>"+element.id+"</td>"
+        let nomb="<td>"+element.id+"</td>"
+        let oferta="<td>"+element.id+"</td>"
+        let precio="<td>"+element.id+"</td>"
+    });
+}
