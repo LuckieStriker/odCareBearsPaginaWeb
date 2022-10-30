ade17f621c76a9596cb204dd75de0234107ac825
Fernanda Palapa <fernandabqd@gmail.com>
2022-10-30 12:32:19 -0600

    refactorizacion function

--- js/detalles-producto.js
+++ js/detalles-producto.js
@@ -1,14 +1,4 @@
-var ImagenPrincipal = document.getElementById('ImagenPrincipal');
-var ImagenSmall = document.getElementsByClassName('small-img');
-ImagenSmall[0].onclick=function(){
-    ImagenPrincipal.src = ImagenSmall[0].src;
-}
-ImagenSmall[1].onclick=function(){
-    ImagenPrincipal.src = ImagenSmall[1].src;
-}
-ImagenSmall[2].onclick=function(){
-    ImagenPrincipal.src = ImagenSmall[2].src;
-}
-ImagenSmall[3].onclick=function(){
-    ImagenPrincipal.src = ImagenSmall[3].src;
+function cambioImg(smallImg){
+    var fullImg=document.getElementById("CajaImg");
+    fullImg.src=smallImg.src;
 }
