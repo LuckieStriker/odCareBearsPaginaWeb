9ea623332ca67997930c74affe9d05c1e40ca7e3
Fernanda Palapa <fernandabqd@gmail.com>
2022-10-29 23:54:20 -0500

    add: descripcion del producto

--- js/detalles-producto.js
+++ js/detalles-producto.js
@@ -1,4 +1,14 @@
-function myFuntion(smaLLImg){
-    var fullImg=document.getElementById("imageBox");
-    fullImg.src=smaLLImg.src;
+var ImagenPrincipal = document.getElementById('ImagenPrincipal');
+var ImagenSmall = document.getElementsByClassName('small-img');
+ImagenSmall[0].onclick=function(){
+    ImagenPrincipal.src = ImagenSmall[0].src;
+}
+ImagenSmall[1].onclick=function(){
+    ImagenPrincipal.src = ImagenSmall[1].src;
+}
+ImagenSmall[2].onclick=function(){
+    ImagenPrincipal.src = ImagenSmall[2].src;
+}
+ImagenSmall[3].onclick=function(){
+    ImagenPrincipal.src = ImagenSmall[3].src;
 }
