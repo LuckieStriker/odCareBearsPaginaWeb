8e3580db142c6838e1877f9d4a3296a054d6e816
vivancomiguel <113225991+vivancomiguel@users.noreply.github.com>
2022-11-02 19:54:36 -0600

    fix: Detalles carrusel

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,15 +1,17 @@
-let items = document.querySelectorAll('.carousel .carousel-item')
+(function() {
+    let items = document.querySelectorAll('.carousel .carousel-item')
 
-items.forEach((el) => {
-    const minPerSlide = 3;
-    let next = el.nextElementSibling;
-    for (var i=1; i<minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
-        	next = items[0]
-      	}
-        let cloneChild = next.cloneNode(true);
-        el.appendChild(cloneChild.children[0]);
-        next = next.nextElementSibling;
-    }
-})
+    items.forEach((el) => {
+        const minPerSlide = 3
+        let next = el.nextElementSibling
+        for (var i=1; i<minPerSlide; i++) {
+            if (!next) {
+                // wrap carousel by using first child
+                next = items[0]
+            }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling
+        }
+    })
+})();
