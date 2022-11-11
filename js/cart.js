acc8db8562566a3da3b1a22689eddea51950dd0a
audrey juarez <audrey1.juarez@gmail.com>
2022-11-10 21:43:40 -0600

    fix: contador de carrito

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,4 @@
+import { navCartItemNumber } from "/js/nav.js";
 const products = [
     {
         "categoria": "ositos",
@@ -10,8 +11,8 @@ const products = [
                 "descuento": 100.00,
                 "imagen": "/assets/img/Ositos/OsoMorado.jpg",
                 "imagenSec": "/assets/img/Ositos/OsoMorado1.jpg",
-                "imagenTer":"/assets/img/Ositos/OsoMorado2.jpg",
-                "descripcion":"Mochilita cariñosita con correas ajustables y cierre trasero para guardar lo que desees",
+                "imagenTer": "/assets/img/Ositos/OsoMorado2.jpg",
+                "descripcion": "Mochilita cariñosita con correas ajustables y cierre trasero para guardar lo que desees",
                 "id": 1
             },
             {
@@ -21,8 +22,8 @@ const products = [
                 "descuento": 100.00,
                 "imagen": "/assets/img/Ositos/OsoAzul.jpg",
                 "imagenSec": "/assets/img/Ositos/OsoAzul1.jpg",
-                "imagenTer":"/assets/img/Ositos/OsitoAzul3.jpg",
-                "descripcion":"Osito Dormiloncito, con su corazón intacto para que le des mucho amor, ojitos cansaditos y bonitos",
+                "imagenTer": "/assets/img/Ositos/OsitoAzul3.jpg",
+                "descripcion": "Osito Dormiloncito, con su corazón intacto para que le des mucho amor, ojitos cansaditos y bonitos",
                 "id": 2
             },
             {
@@ -33,7 +34,7 @@ const products = [
                 "imagen": "/assets/img/Ositos/OsoCafe.jpg",
                 "imagenSec": "/assets/img/Ositos/OsoCafe1.jpg",
                 "imagenTer": "/assets/img/Ositos/OsoCafe2.jpg",
-                "descripcion":"Tiernosito café esta listo para que acaricies, y cuides de él",
+                "descripcion": "Tiernosito café esta listo para que acaricies, y cuides de él",
                 "id": 3
             },
             {
@@ -44,7 +45,7 @@ const products = [
                 "imagen": "/assets/img/Ositos/OsoRosa.jpg",
                 "imagenSec": "/assets/img/Ositos/OsoRosa1.jpg",
                 "imagenTer": "/assets/img/Ositos/OsoRosa2.jpg",
-                "descripcion":"Osito con mucho amor para dar y que cuides de él, en perfecto estado. ",
+                "descripcion": "Osito con mucho amor para dar y que cuides de él, en perfecto estado. ",
                 "id": 4
             },
             {
@@ -55,7 +56,7 @@ const products = [
                 "imagen": "/assets/img/Ositos/OsoRosita.jpg",
                 "imagenSec": "/assets/img/Ositos/OsoRosita1.jpg",
                 "imagenTer": "/assets/img/Ositos/OsoRosita2.jpg",
-                "descripcion":"Bebé de los cariñositos, adaptable a mochila, sin daños",
+                "descripcion": "Bebé de los cariñositos, adaptable a mochila, sin daños",
                 "id": 5
             },
 
@@ -74,7 +75,7 @@ const products = [
                 "imagen": "/assets/img/Accesorios/Estrellitas.jpg",
                 "imagenSec": "/assets/img/Accesorios/Estrellitas4.jpg",
                 "imagenTer": "/assets/img/Accesorios/Estrellitas3.jpg",
-                "descripcion":"Hermosos aretes en dorado y azul, figuras de planeta y luna con estrellas, con moños azul, pequeños y discretos ",
+                "descripcion": "Hermosos aretes en dorado y azul, figuras de planeta y luna con estrellas, con moños azul, pequeños y discretos ",
                 "id": 6
             },
             {
@@ -85,7 +86,7 @@ const products = [
                 "imagen": "/assets/img/Accesorios/AretesLunaPlaneta.jpg",
                 "imagenSec": "/assets/img/Accesorios/AretesLunaPlaneta2.jpg",
                 "imagenTer": "/assets/img/Accesorios/AretesLunaPlaneta3.jpg",
-                "descripcion":"Hermosos aretes en dorado y azul en forma circular, con imagen de la luna, estrellas y un planeta.",
+                "descripcion": "Hermosos aretes en dorado y azul en forma circular, con imagen de la luna, estrellas y un planeta.",
                 "id": 7
             },
             {
@@ -96,10 +97,10 @@ const products = [
                 "imagen": "/assets/img/Accesorios/LunaEstrellita.jpg",
                 "imagenSec": "/assets/img/Accesorios/LunaEstrellita2.jpg",
                 "imagenTer": "/assets/img/Accesorios/EstrellitasPerla.jpg",
-                "descripcion":"Hermosos aretes en dorado y azul, una luna y una estrella con una perla.",
+                "descripcion": "Hermosos aretes en dorado y azul, una luna y una estrella con una perla.",
                 "id": 8
             },
-            
+
             {
                 "categoria": "Accesorios",
                 "nombre": "Noche Estrellada ",
@@ -108,7 +109,7 @@ const products = [
                 "imagen": "/assets/img/Accesorios/NocheEstrellada1.jpg",
                 "imagenSec": "/assets/img/Accesorios/NocheEstrellada2.jpg",
                 "imagenTer": "/assets/img/Accesorios/NocheEstrellada1.jpg",
-                "descripcion":"Hermosos aretes en dorado y azul, una luna llena en aros, con estrellas y planetas pequeños colgantes.",
+                "descripcion": "Hermosos aretes en dorado y azul, una luna llena en aros, con estrellas y planetas pequeños colgantes.",
                 "id": 9
             },
 
@@ -127,7 +128,7 @@ const products = [
                 "imagen": "/assets/img/Ceramica/Alajero.jpg",
                 "imagenSec": "/assets/img/Ceramica/Alajero1.jpg",
                 "imagenTer": "/assets/img/Ceramica/Alajero2.jpg",
-                "descripcion":"Alajero de ceramica con flores, bordes dorados en la base.",
+                "descripcion": "Alajero de ceramica con flores, bordes dorados en la base.",
                 "id": 10
             },
             {
@@ -138,7 +139,7 @@ const products = [
                 "imagen": "/assets/img/Ceramica/CajitaPastel.jpg",
                 "imagenSec": "/assets/img/Ceramica/CajitaPastel1.jpg",
                 "imagenTer": "/assets/img/Ceramica/CajitaPastel2.jpg",
-                "descripcion":"Pastelito,con fresas en la parte de arriba,puedes guardar aretes y collares en los compartimientos internos. <br> Fue pintado y moldeado a mano.<br> **Incluye 2 macarons de Ositos.",
+                "descripcion": "Pastelito,con fresas en la parte de arriba,puedes guardar aretes y collares en los compartimientos internos. <br> Fue pintado y moldeado a mano.<br> **Incluye 2 macarons de Ositos.",
                 "id": 11
             },
             {
@@ -149,7 +150,7 @@ const products = [
                 "imagen": "/assets/img/Ceramica/PlatoLargo.jpg",
                 "imagenSec": "/assets/img/Ceramica/PlatoLargo2.jpg",
                 "imagenTer": "/assets/img/Ceramica/PlatoLargo.jpg",
-                "descripcion":"Platón de ceramica, con la imagen de Cinnamoroll pintado a mano, con técnica de hoja de oro.",
+                "descripcion": "Platón de ceramica, con la imagen de Cinnamoroll pintado a mano, con técnica de hoja de oro.",
                 "id": 12
             },
             {
@@ -160,7 +161,7 @@ const products = [
                 "imagen": "/assets/img/Ceramica/Taza.jpg",
                 "imagenSec": "/assets/img/Ceramica/Taza2.jpg",
                 "imagenTer": "/assets/img/Ceramica/Taza1.jpg",
-                "descripcion":"Pequeño alajero de My Melody, se asoma su figura.",
+                "descripcion": "Pequeño alajero de My Melody, se asoma su figura.",
                 "id": 13
             },
 
@@ -178,7 +179,7 @@ const products = [
                 "imagen": "/assets/img/Otros/Pintura1.jpg",
                 "imagenSec": "/assets/img/Otros/Pintura2.jpg",
                 "imagenTer": "/assets/img/Otros/Pintura1.jpg",
-                "descripcion":"Pintura con técnica sobre oleo",
+                "descripcion": "Pintura con técnica sobre oleo",
                 "id": 14
             },
             {
@@ -189,7 +190,7 @@ const products = [
                 "imagen": "/assets/img/Otros/Pintura1.jpg",
                 "imagenSec": "/assets/img/Otros/Pintura2.jpg",
                 "imagenTer": "/assets/img/Otros/Pintura1.jpg",
-                "descripcion":"Pintura con técnica sobre oleo",
+                "descripcion": "Pintura con técnica sobre oleo",
                 "id": 15
             }
 
@@ -197,33 +198,34 @@ const products = [
     }
 ]
 
-let storedCart = JSON.parse(localStorage.getItem("productosCarrito"));
-let html="";
+
+let html = "";
 let total = 0;
 renderCart();
 
 function renderCart() {
-
+    let storedCart = JSON.parse(localStorage.getItem("productosCarrito"));
     const ids = [];
     storedCart.forEach(prod => ids.push(prod.id));
 
-    for (let i=0; i<products.length; i++){
+    for (let i = 0; i < products.length; i++) {
         products[i].productos.forEach(element => {
-           //if(ids.includes(String(element.id))) console.log(element, storedCart[ids.indexOf(String(element.id))].cant)
-           
-           if(ids.includes(String(element.id))) {
-            let cantidad = storedCart[ids.indexOf(String(element.id))].cant;
-            renderProduct(element, cantidad)
+
+            if (ids.includes(String(element.id))) {
+                let cantidad = storedCart[ids.indexOf(String(element.id))].cant;
+                renderProduct(element, cantidad)
             }
-            });
-            document.getElementById('fila-1').innerHTML = html;
-            
-}
-document.getElementById("totalCarrito").innerHTML = `<h5> Total: $${total} MXN </h5>`;
-console.log(total);
+        });
+        document.getElementById('fila-1').innerHTML = html;
+
+    }
+    document.querySelectorAll("input").forEach(cant_input => cant_input.addEventListener('input', refreshCart));
+    document.querySelectorAll(".borrar").forEach(borrarbtn => borrarbtn.addEventListener('click', borrarArt));
+
+    document.getElementById("totalCarrito").innerHTML = `<h5> Total: $${total} MXN </h5>`;
 }
 
-function renderProduct(prod, cant){
+function renderProduct(prod, cant) {
     let fila = `<div class="card mb-4">
     <div class="container">
       <div class="row">
@@ -239,10 +241,10 @@ function renderProduct(prod, cant){
         </div>
       
         <div class="col my-auto text-center">
-          <input type="number" name="${prod.id}" value = "${cant}" min="0" pattern="^[0-50]"> </input>
+          <input type="number" name="${prod.id}" value = "${cant}" min="1" pattern="^[1-50]"> </input>
         </div>
         <div class="col my-auto text-center">
-          <b class="">$${prod.precio-prod.descuento} MXN</b>
+          <b class="">$${prod.precio - prod.descuento} MXN</b>
         </div>
         <div class="col my-auto text-center">
         <a href="#!"><img src="/assets/img/trash.svg" name="${prod.id}" class="borrar"></a>
@@ -250,56 +252,52 @@ function renderProduct(prod, cant){
       </div>
     </div>
   </div>`;
-  html += fila;
-  total += parseInt(prod.precio-prod.descuento)*cant;
+    html += fila;
+    total += parseInt(prod.precio - prod.descuento) * cant;
 }
 
 document.querySelectorAll("input").forEach(cant_input => cant_input.addEventListener('input', refreshCart));
 document.querySelectorAll(".borrar").forEach(borrarbtn => borrarbtn.addEventListener('click', borrarArt));
 
-function refreshCart(){
-let newCart = [];
-let productId = this.name;
-let flag = false;
-total = 0;
-if (storedCart){
-    newCart = storedCart.slice();
-    newCart.forEach(product => {
-        if(product.id == productId){
-             product.cant = this.value;
-             flag = true;
+function refreshCart() {
+    let storedCart = JSON.parse(localStorage.getItem("productosCarrito"));
+    let newCart = [];
+    let productId = this.name;
+    let flag = false;
+    total = 0;
+    if (storedCart) {
+        newCart = storedCart.slice();
+        newCart.forEach(product => {
+            if (product.id == productId) {
+                product.cant = this.value;
+                flag = true;
             }
         });
-        if (!flag) newCart.push({id: productId, cant: this.value});
     }
-    
-    else {
-        newCart.push(cart);
-    } ;
+
     localStorage.setItem("productosCarrito", JSON.stringify(newCart));
+    html = "";
     renderCart();
 }
 
 function borrarArt() {
+    let storedCart = JSON.parse(localStorage.getItem("productosCarrito"));
     let newCart = [];
     let productId = this.name;
-    console.log(productId);
-    let flag = false;
     total = 0;
-    if (storedCart){
+    if (storedCart) {
         let count = 0;
         newCart = storedCart.slice();
         newCart.forEach(product => {
-            if(product.id == productId){
-                newCart = newCart.splice(count,1);
-                console.log(newCart);
-                }
-                count++;
-            });
-        }
-         ;
-        localStorage.setItem("productosCarrito", JSON.stringify(newCart));
-        document.getElementById('fila-1').innerHTML = html;
-        html ='';
-        renderCart();
+            if (product.id == productId) {
+                newCart.splice(count, 1);
+            }
+            count++;
+        });
+    }
+    ;
+    localStorage.setItem("productosCarrito", JSON.stringify(newCart));
+    html = '';
+    renderCart();
+    navCartItemNumber();
 }
