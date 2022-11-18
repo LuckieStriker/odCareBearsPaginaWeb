1c1486f85fba06b1a7ad9334347829f9a8c919f5
Fernanda Palapa <fernandabqd@gmail.com>
2022-11-18 09:14:48 -0600

    conexion api en productos

--- js/detalles-producto.js
+++ js/detalles-producto.js
@@ -1,202 +1,3 @@
-const products = [
-    {
-        "categoria": "ositos",
-        "nombre": "Ositos Cariñositos",
-        "productos": [
-            {
-                "categoria": "ositos",
-                "nombre": "Oso Morado",
-                "precio": 199.00,
-                "descuento": 100.00,
-                "imagen": "/assets/img/Ositos/OsoMorado.jpg",
-                "imagenSec": "/assets/img/Ositos/OsoMorado1.jpg",
-                "imagenTer":"/assets/img/Ositos/OsoMorado2.jpg",
-                "descripcion":"Mochilita cariñosita con correas ajustables y cierre trasero para guardar lo que desees",
-                "id": 1
-            },
-            {
-                "categoria": "ositos",
-                "nombre": "Oso Dormiloncito",
-                "precio": 199.00,
-                "descuento": 100.00,
-                "imagen": "/assets/img/Ositos/OsoAzul.jpg",
-                "imagenSec": "/assets/img/Ositos/OsoAzul1.jpg",
-                "imagenTer":"/assets/img/Ositos/OsitoAzul3.jpg",
-                "descripcion":"Osito Dormiloncito, con su corazón intacto para que le des mucho amor, ojitos cansaditos y bonitos",
-                "id": 2
-            },
-            {
-                "categoria": "ositos",
-                "nombre": "Tiernosito café",
-                "precio": 199.00,
-                "descuento": 100.00,
-                "imagen": "/assets/img/Ositos/OsoCafe.jpg",
-                "imagenSec": "/assets/img/Ositos/OsoCafe1.jpg",
-                "imagenTer": "/assets/img/Ositos/OsoCafe2.jpg",
-                "descripcion":"Tiernosito café esta listo para que acaricies, y cuides de él",
-                "id": 3
-            },
-            {
-                "categoria": "ositos",
-                "nombre": "Love A Lot Bear",
-                "precio": 340.00,
-                "descuento": 100.00,
-                "imagen": "/assets/img/Ositos/OsoRosa.jpg",
-                "imagenSec": "/assets/img/Ositos/OsoRosa1.jpg",
-                "imagenTer": "/assets/img/Ositos/OsoRosa2.jpg",
-                "descripcion":"Osito con mucho amor para dar y que cuides de él, en perfecto estado. ",
-                "id": 4
-            },
-            {
-                "categoria": "ositos",
-                "nombre": "Bebesito cariñosito rosita",
-                "precio": 349.00,
-                "descuento": 100.00,
-                "imagen": "/assets/img/Ositos/OsoRosita.jpg",
-                "imagenSec": "/assets/img/Ositos/OsoRosita1.jpg",
-                "imagenTer": "/assets/img/Ositos/OsoRosita2.jpg",
-                "descripcion":"Bebé de los cariñositos, adaptable a mochila, sin daños",
-                "id": 5
-            },
-
-
-        ]
-    },
-    {
-        "categoria": "Accesorios",
-        "nombre": "Accesorios",
-        "productos": [
-            {
-                "categoria": "Accesorios",
-                "nombre": "Aretes Estrella ",
-                "precio": 199.00,
-                "descuento": 100.00,
-                "imagen": "/assets/img/Accesorios/Estrellitas.jpg",
-                "imagenSec": "/assets/img/Accesorios/Estrellitas4.jpg",
-                "imagenTer": "/assets/img/Accesorios/Estrellitas3.jpg",
-                "descripcion":"Hermosos aretes en dorado y azul, figuras de planeta y luna con estrellas, con moños azul, pequeños y discretos ",
-                "id": 6
-            },
-            {
-                "categoria": "Accesorios",
-                "nombre": "Aretes Aros Luna-Planeta ",
-                "precio": 199.00,
-                "descuento": 100.00,
-                "imagen": "/assets/img/Accesorios/AretesLunaPlaneta.jpg",
-                "imagenSec": "/assets/img/Accesorios/AretesLunaPlaneta2.jpg",
-                "imagenTer": "/assets/img/Accesorios/AretesLunaPlaneta3.jpg",
-                "descripcion":"Hermosos aretes en dorado y azul en forma circular, con imagen de la luna, estrellas y un planeta.",
-                "id": 7
-            },
-            {
-                "categoria": "Accesorios",
-                "nombre": "Aretes Luna-Estrella ",
-                "precio": 199.00,
-                "descuento": 100.00,
-                "imagen": "/assets/img/Accesorios/LunaEstrellita.jpg",
-                "imagenSec": "/assets/img/Accesorios/LunaEstrellita2.jpg",
-                "imagenTer": "/assets/img/Accesorios/EstrellitasPerla.jpg",
-                "descripcion":"Hermosos aretes en dorado y azul, una luna y una estrella con una perla.",
-                "id": 8
-            },
-            
-            {
-                "categoria": "Accesorios",
-                "nombre": "Noche Estrellada ",
-                "precio": 199.00,
-                "descuento": 100.00,
-                "imagen": "/assets/img/Accesorios/NocheEstrellada1.jpg",
-                "imagenSec": "/assets/img/Accesorios/NocheEstrellada2.jpg",
-                "imagenTer": "/assets/img/Accesorios/NocheEstrellada1.jpg",
-                "descripcion":"Hermosos aretes en dorado y azul, una luna llena en aros, con estrellas y planetas pequeños colgantes.",
-                "id": 9
-            },
-
-        ]
-    },
-    {
-        "categoria": "ceramica",
-        "nombre": "Cerámica",
-        "productos": [
-
-            {
-                "categoria": "ceramica",
-                "nombre": "Alajero",
-                "precio": 390.00,
-                "descuento": 100.00,
-                "imagen": "/assets/img/Ceramica/Alajero.jpg",
-                "imagenSec": "/assets/img/Ceramica/Alajero1.jpg",
-                "imagenTer": "/assets/img/Ceramica/Alajero2.jpg",
-                "descripcion":"Alajero de ceramica con flores, bordes dorados en la base.",
-                "id": 10
-            },
-            {
-                "categoria": "ceramica",
-                "nombre": "Cajita pastel",
-                "precio": 590.00,
-                "descuento": 100.00,
-                "imagen": "/assets/img/Ceramica/CajitaPastel.jpg",
-                "imagenSec": "/assets/img/Ceramica/CajitaPastel1.jpg",
-                "imagenTer": "/assets/img/Ceramica/CajitaPastel2.jpg",
-                "descripcion":"Pastelito,con fresas en la parte de arriba,puedes guardar aretes y collares en los compartimientos internos. <br> Fue pintado y moldeado a mano.<br> **Incluye 2 macarons de Ositos.",
-                "id": 11
-            },
-            {
-                "categoria": "ceramica",
-                "nombre": "Plato largo",
-                "precio": 475.00,
-                "descuento": 100.00,
-                "imagen": "/assets/img/Ceramica/PlatoLargo.jpg",
-                "imagenSec": "/assets/img/Ceramica/PlatoLargo2.jpg",
-                "imagenTer": "/assets/img/Ceramica/PlatoLargo.jpg",
-                "descripcion":"Platón de ceramica, con la imagen de Cinnamoroll pintado a mano, con técnica de hoja de oro.",
-                "id": 12
-            },
-            {
-                "categoria": "ceramica",
-                "nombre": "Alajero My Melody",
-                "precio": 199.00,
-                "descuento": 100.00,
-                "imagen": "/assets/img/Ceramica/Taza.jpg",
-                "imagenSec": "/assets/img/Ceramica/Taza2.jpg",
-                "imagenTer": "/assets/img/Ceramica/Taza1.jpg",
-                "descripcion":"Pequeño alajero de My Melody, se asoma su figura.",
-                "id": 13
-            },
-
-        ]
-    },
-    {
-        "categoria": "otros",
-        "nombre": "Otros",
-        "productos": [
-            {
-                "categoria": "otros",
-                "nombre": "Pintura De Oso",
-                "precio": 299.00,
-                "descuento": 100.00,
-                "imagen": "/assets/img/Otros/Pintura1.jpg",
-                "imagenSec": "/assets/img/Otros/Pintura2.jpg",
-                "imagenTer": "/assets/img/Otros/Pintura1.jpg",
-                "descripcion":"Pintura con técnica sobre oleo",
-                "id": 14
-            },
-            {
-                "categoria": "otros",
-                "nombre": "Pintura De Oso",
-                "precio": 299.00,
-                "descuento": 100.00,
-                "imagen": "/assets/img/Otros/Pintura1.jpg",
-                "imagenSec": "/assets/img/Otros/Pintura2.jpg",
-                "imagenTer": "/assets/img/Otros/Pintura1.jpg",
-                "descripcion":"Pintura con técnica sobre oleo",
-                "id": 15
-            }
-
-        ]
-    }
-]
-
 
 function cambioImg(smallImg) {
     let fullImg = document.getElementById("CajaImg");
@@ -212,13 +13,35 @@ function getParameterByName(name, url = window.location.href) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 let ID1234 = getParameterByName('id');
+solicitarDatos(ID1234);
 
 
-for (i=0; i<products.length; i++){
-products[i].productos.forEach(element => {
-   if(ID1234 == element.id) nuevoProducto(element) 
-    });
-}
+let data
+  
+function solicitarDatos(id){
+    let myHeaders = new Headers();
+myHeaders.append('Content-Type','application/json;charset=UTF-8');
+    fetch(`https://odcarebears-backend-production.up.railway.app/api/producto/${id}`, myHeaders)
+    .then(respuesta=>respuesta.json())
+        .then(cuerpo=>{
+            console.log(cuerpo)
+     data=
+    {
+        "nombre": cuerpo.nombreProducto,
+        "descripcion": cuerpo.descripcion,
+        "categoria": cuerpo.idCategoria.nombreCategoria,
+        "precio": cuerpo.precio,
+        "descuento": cuerpo.precio*0.1 ,
+        "imagen": cuerpo.foto,
+        "imagenSec": cuerpo.foto2,
+        "imagenTer": cuerpo.foto3,
+    } 
+    nuevoProducto(data)
+} )
+    .catch(error => console.log(error))
+  }
+  
+
 function nuevoProducto(element){
 X = document.getElementById("miProducto");
 X.innerHTML = `
